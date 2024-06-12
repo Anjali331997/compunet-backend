a96599756a0c6c8e5c25fd628bdacab75e0edf50
Anjali331997 <anjaliap3march1997@gmail.com>
2024-06-12 10:16:22 +0530

    connected to database complete

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const express = require('express')
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+
 require('dotenv').config()
-
+require('./config/db')
 const port = process.env.PORT || 4000;
-const app = express();
 
+const app = express();
+app.use(bodyparser.json());
+app.use(express.json())
+app.use(cors());
+
+
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        message:"compunet basic backend setip complete."
+    })
+})
 
 app.listen(port, () => {
     console.log(`Listening to port ${port}`);
